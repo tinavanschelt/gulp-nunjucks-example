5a6ea5cdee7260dbfb48c0b237bbb01089457006
Tina van Schelt <tina@babylonstoren.com>
2018-03-17 14:57:00 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp');
+
+var data = require('gulp-data');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('nunjucks', function() {
+    // Gets .html and .nunjucks files in pages
+    return gulp.src('app/pages/**/*.+(html|nunjucks)')
+    // Adding data to Nunjucks
+    .pipe(data(function() {
+        return require('./app/data.json')
+      }))
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+        path: ['app/templates']
+        }))
+    // output files in app folder
+    .pipe(gulp.dest('app'))
+});
